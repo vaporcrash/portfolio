3f904daaa45a525ddcd1fc16c0caa4b4ec449a39
Sharvan Kumaran <shar@Sharvans-MacBook-Air.local>
2026-02-06 14:38:46 -0800

    Title spacing

--- src/components/WorkExp.jsx
+++ src/components/WorkExp.jsx
@@ -3,11 +3,11 @@ import {Animator,ScrollPage,Sticky,batch,MoveIn, Fade} from 'react-scroll-motion
 
 function WorkExp() {
   return <ScrollPage className='Even'>
-        <Animator animation={batch(MoveIn(0,-100),Fade(0,1),Sticky(50,10))}>
+        <Animator animation={batch(MoveIn(0,-100),Fade(0,1),Sticky(50,20))}>
             <p id='SkillPageTitle'>Work Experience</p>
             <p id='SkillPageTitle'>LTIMindtree</p>
         </Animator>
-        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(30,45))}>
+        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(30,55))}>
             <div className='Web3-text'>
             <p className='Skills-title'>OneWAN Deployment</p>
             <p className='Skills'>Worked on onboarding Cisco and Arista devices to Microsoft Azure's SDWAN</p>
@@ -15,7 +15,7 @@ function WorkExp() {
             <p className='Skills'>Worked with storage and database solutions like Kusto and Git to story and query network and device state information</p>
             </div>
         </Animator>
-        <Animator animation={batch(MoveIn(700,0),Fade(0,1),Sticky(70,45))}>
+        <Animator animation={batch(MoveIn(700,0),Fade(0,1),Sticky(70,55))}>
             <div className='Web3-text'>
             <p className='Skills-title'>Azure for Operators</p>
             <p className='Skills'> Worked on bringing up and maintaining Kubernetes clusters, including Hardware configurations using Airship (Cluster Lifecycle Management), and troubleshooting cluster issues with the kubectl tool</p>
