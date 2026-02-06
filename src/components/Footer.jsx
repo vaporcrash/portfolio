f539070b3eb35eb6f9d70679e0c09ca87a8098ad
Sharvan Kumaran <shar@Sharvans-MacBook-Air.local>
2026-02-06 01:49:47 -0800

    Resume updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 function Footer(){
     return <footer className='Landing-text' style={{textAlign:'center'}}>
-        <SocialIcon url='https://www.linkedin.com/in/sharvan-kumaran/' target='_blank'/> <SocialIcon url='https://github.com/vaporcrash' bgColor='gray' target='_blank'/> <SocialIcon url='https://open.spotify.com/user/22yr72sc5gczjie2dvves53ji?si=gCEPKSsrSMiQ_vtAVgZ49A' target='_blank' /> <SocialIcon url='https://stackoverflow.com/users/21475533/sharvan-kumaran/' target='_blank' /> <p style={{paddingLeft:'0.5vh'}}><a className='jumps' href="https://drive.google.com/file/d/1pcIKLGIqlPMCAhOQ1UDja0kqr1q6EHQF/view?usp=sharing" target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faEnvelope} size="xl" />-Resume</a></p>
+        <SocialIcon url='https://www.linkedin.com/in/sharvan-kumaran/' target='_blank'/> <SocialIcon url='https://github.com/vaporcrash' bgColor='gray' target='_blank'/> <SocialIcon url='https://open.spotify.com/user/22yr72sc5gczjie2dvves53ji?si=gCEPKSsrSMiQ_vtAVgZ49A' target='_blank' /> <SocialIcon url='https://stackoverflow.com/users/21475533/sharvan-kumaran/' target='_blank' /> <p style={{paddingLeft:'0.5vh'}}><a className='jumps' href="https://drive.google.com/file/d/1yshrRit7WMmXlPbIM1Mf8OMlrNP02nZQ/view?usp=drive_link" target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faEnvelope} size="xl" />-Resume</a></p>
     </footer>
 }
 
