9db52c0a52ad04e3df698da22433abd1a68fbb2d
Sharvan Kumaran <shar@Sharvans-MacBook-Air.local>
2026-02-06 14:37:32 -0800

    Spacing updated

--- src/components/WorkExp.jsx
+++ src/components/WorkExp.jsx
@@ -7,7 +7,7 @@ function WorkExp() {
             <p id='SkillPageTitle'>Work Experience</p>
             <p id='SkillPageTitle'>LTIMindtree</p>
         </Animator>
-        <Animator animation={batch(MoveIn(700,0),Fade(0,1),Sticky(70,30))}>
+        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(30,45))}>
             <div className='Web3-text'>
             <p className='Skills-title'>OneWAN Deployment</p>
             <p className='Skills'>Worked on onboarding Cisco and Arista devices to Microsoft Azure's SDWAN</p>
@@ -15,7 +15,7 @@ function WorkExp() {
             <p className='Skills'>Worked with storage and database solutions like Kusto and Git to story and query network and device state information</p>
             </div>
         </Animator>
-        <Animator animation={batch(MoveIn(-700,0),Fade(0,1),Sticky(30,45))}>
+        <Animator animation={batch(MoveIn(700,0),Fade(0,1),Sticky(70,45))}>
             <div className='Web3-text'>
             <p className='Skills-title'>Azure for Operators</p>
             <p className='Skills'> Worked on bringing up and maintaining Kubernetes clusters, including Hardware configurations using Airship (Cluster Lifecycle Management), and troubleshooting cluster issues with the kubectl tool</p>
